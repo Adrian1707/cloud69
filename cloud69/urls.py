28b06bfc5eec44eef6f491ab4c39deb8d970650c
Adrian Booth <adrian.booth17@gmail.com>
2019-03-31 21:48:38 +0100

    add route for deleting all instances and key pairs

--- cloud69/urls.py
+++ cloud69/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('create', views.create, name='create'),
+    path('delete_all', views.delete_all, name='delete_all'),
 ]
--- cloud69/views.py
+++ cloud69/views.py
@@ -48,6 +48,34 @@ def create(request):
     template = loader.get_template('cloud69/success.html')
     return HttpResponse(template.render({}, request))
 
+def delete_all(request):
+    ec2 = boto3.resource(
+        "ec2",
+        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+        region_name="eu-west-2"
+        )
+
+    client = boto3.client(
+        "ec2",
+        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+        region_name="eu-west-2"
+    )
+
+    ec2.instances.terminate()
+
+    keypairs = client.describe_key_pairs()["KeyPairs"]
+    for pair in keypairs:
+        response = client.delete_key_pair(
+            KeyName=pair['KeyName'],
+        )
+
+    template = loader.get_template('cloud69/success.html')
+    return HttpResponse(template.render({}, request))
+
+
+
 def randomString(stringLength=10):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
