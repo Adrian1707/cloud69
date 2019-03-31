5a9dcb5250be4af05e6f8581a8746ce219bbfcfc
Adrian Booth <adrian.booth17@gmail.com>
2019-03-31 17:13:36 +0100

    install boto3 to talk to AWS

--- cloud69/views.py
+++ cloud69/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
 from django.template import loader
+import os
+import boto3
+
 
 def index(request):
+    client = boto3.client(
+        's3',
+        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+    )
+    s3_buckets = client.list_buckets()["Buckets"]
+    for bucket in s3_buckets:
+        print(bucket["Name"])
     template = loader.get_template('cloud69/index.html')
     return HttpResponse(template.render({}, request))
 
