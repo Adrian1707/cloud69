2e437df8e2e0090ebd61e400eca2e812aad68e3f
Adrian Booth <adrian.booth17@gmail.com>
2019-04-05 16:56:30 +0100

    Refactor views and place EC2 api logic into seperate file

--- cloud69/views.py
+++ cloud69/views.py
@@ -1,81 +1,21 @@
 from django.http import HttpResponse
 from django.template import loader
-import os
-import boto3
-import random
-import string
+from cloud69.ec2_client import *
+import pdb
 
 def index(request):
-    client = boto3.client(
-        's3',
-        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
-    )
-    s3_buckets = client.list_buckets()["Buckets"]
-    for bucket in s3_buckets:
-        print(bucket["Name"])
     template = loader.get_template('cloud69/index.html')
     return HttpResponse(template.render({}, request))
 
 def create(request):
-    ec2 = boto3.resource(
-        "ec2",
-        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
-        region_name="eu-west-2"
-        )
-
-    key_name = randomString()
-
-    # create a file to store key locally
-    outfile = open("{key_name}.pem".format(key_name=key_name), 'w')
-
-    # call the boto ecw function to create a key pair
-    key_pair = ec2.create_key_pair(KeyName=key_name)
-    #
-    # # capture the key and store it in a file
-    KeyPairout = str(key_pair.key_material)
-    outfile.write(KeyPairout)
-
-    ec2.create_instances(
-        ImageId="ami-09ead922c1dad67e4",
-        MinCount=1,
-        MaxCount=1,
-        InstanceType='t2.micro',
-        KeyName=key_name
-    )
-
+    EC2Client().create_ec2_instance()
     template = loader.get_template('cloud69/success.html')
     return HttpResponse(template.render({}, request))
 
 def delete_all(request):
-    ec2 = boto3.resource(
-        "ec2",
-        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
-        region_name="eu-west-2"
-        )
-
-    client = boto3.client(
-        "ec2",
-        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
-        region_name="eu-west-2"
-    )
-
-    ec2.instances.terminate()
-
-    keypairs = client.describe_key_pairs()["KeyPairs"]
-    for pair in keypairs:
-        response = client.delete_key_pair(
-            KeyName=pair['KeyName'],
-        )
-
+    EC2Client().delete_all_ec2_instances()
     template = loader.get_template('cloud69/success.html')
     return HttpResponse(template.render({}, request))
 
 
 
-def randomString(stringLength=10):
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(stringLength))
