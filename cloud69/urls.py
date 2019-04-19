41b8c7dfd738ea59110fef9b88adbb4d9681a119
Adrian Booth <adrian.booth17@gmail.com>
2019-04-19 19:24:19 +0800

    cleanup views.py and place cloud formation calls in new cloud formation client

--- cloud69/urls.py
+++ cloud69/urls.py
@@ -4,9 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('create', views.create, name='create'),
-    path('delete_all', views.delete_all, name='delete_all'),
-    path('delete', views.delete, name='delete'),
     path('new_stack', views.new_stack, name='new_stack'),
     path('create_stack', views.create_stack, name='create_stack'),
 ]
--- cloud69/views.py
+++ cloud69/views.py
@@ -1,64 +1,29 @@
 from django.http import HttpResponse
 from django.template import loader, Context
-from cloud69.ec2_client import *
+from cloud69.cloud_formation_client import *
 from cloud69.models import *
 import json
 import boto3
 import pdb
 
 def index(request):
-    instances = EC2Client().get_instances()
     template = loader.get_template('cloud69/index.html')
     instance_models = []
-    for instance in instances:
-        if instance['State']["Name"] != 'terminated':
-            instance_models.append(EC2Instance.create(instance_response = instance))
 
     html = template.render({'instance_models': instance_models }, request)
     return HttpResponse(html)
 
-def create(request):
-    EC2Client().create_ec2_instance()
-    template = loader.get_template('cloud69/success.html')
-    return HttpResponse(template.render({}, request))
-
-def delete(request):
-    instance_id = request.POST.dict().get("instance_id")
-    EC2Client().delete_instance(instance_id)
-    template = loader.get_template("cloud69/success.html")
-    return HttpResponse(template.render({}, request))
-
-def delete_all(request):
-    EC2Client().delete_all_ec2_instances()
-    template = loader.get_template('cloud69/success.html')
-    return HttpResponse(template.render({}, request))
-
 def new_stack(request):
-    json_string = open("/code/cloud69/static/cloud69/rails_stack.json").read()
-    github_url = "https://github.com/Adrian1707/todo_list"
-    app_name = github_url.split("/")[-1]
-    json_string = json_string.replace('{github_url}', github_url)
-    json_string = json_string.replace('{app_name}', app_name)
-
-    cloud_formation_template_dict = json.loads(json_string)
-    template = loader.get_template("cloud69/new_stack")
-    return HttpResponse(template.render({}, request))
+    template = loader.get_template("cloud69/new_stack.html")
+    return HttpResponse(template.render({"ec2_instance_types": ec2_instance_types, "db_instance_types": db_instance_types}, request))
 
 def create_stack(request):
-    json_string = open("/code/cloud69/static/cloud69/rails_stack.json").read()
-    github_url = request.POST.dict().get("github_url")
-    app_name = github_url.split("/")[-1]
-    json_string = json_string.replace('{github_url}', github_url)
-    json_string = json_string.replace('{app_name}', app_name)
-    client = boto3.client(
-        "cloudformation",
-        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
-        region_name="eu-west-2"
-    )
-    client.create_stack(
-        StackName="TodoList",
-        TemplateBody=json_string,
-        OnFailure='ROLLBACK')
+    CloudFormationClient().create_stack(request.POST.dict())
     template = loader.get_template("cloud69/success.html")
     return HttpResponse(template.render({}, request))
+
+def ec2_instance_types():
+    return ["t1.micro", "t2.nano", "t2.micro", "t2.small", "t2.medium", "t2.large", "m1.small", "m1.medium", "m1.large", "m1.xlarge", "m2.xlarge", "m2.2xlarge", "m2.4xlarge", "m3.medium", "m3.large", "m3.xlarge", "m3.2xlarge", "m4.large", "m4.xlarge", "m4.2xlarge", "m4.4xlarge", "m4.10xlarge", "c1.medium", "c1.xlarge", "c3.large", "c3.xlarge", "c3.2xlarge", "c3.4xlarge", "c3.8xlarge", "c4.large", "c4.xlarge", "c4.2xlarge", "c4.4xlarge", "c4.8xlarge", "g2.2xlarge", "g2.8xlarge", "r3.large", "r3.xlarge", "r3.2xlarge", "r3.4xlarge", "r3.8xlarge", "i2.xlarge", "i2.2xlarge", "i2.4xlarge", "i2.8xlarge", "d2.xlarge", "d2.2xlarge", "d2.4xlarge", "d2.8xlarge", "hi1.4xlarge", "hs1.8xlarge", "cr1.8xlarge", "cc2.8xlarge", "cg1.4xlarge"]
+
+def db_instance_types():
+    return ["db.t1.micro", "db.m1.small", "db.m1.medium", "db.m1.large", "db.m1.xlarge", "db.m2.xlarge", "db.m2.2xlarge", "db.m2.4xlarge", "db.m3.medium", "db.m3.large", "db.m3.xlarge", "db.m3.2xlarge", "db.m4.large", "db.m4.xlarge", "db.m4.2xlarge", "db.m4.4xlarge", "db.m4.10xlarge", "db.r3.large", "db.r3.xlarge", "db.r3.2xlarge", "db.r3.4xlarge", "db.r3.8xlarge", "db.m2.xlarge", "db.m2.2xlarge", "db.m2.4xlarge", "db.cr1.8xlarge", "db.t2.micro", "db.t2.small", "db.t2.medium", "db.t2.large"]
