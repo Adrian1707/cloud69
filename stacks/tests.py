4a605037a071f006d60f8d3a61c81fe43b8f54f8
Adrian Booth <adrian.booth17@gmail.com>
2019-04-29 14:43:46 +0700

    add tests for new stack and home page

--- stacks/tests.py
+++ stacks/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.test import SimpleTestCase
+import pdb
 
-# Create your tests here.
+from . import views
+
+class StacksRoutes(SimpleTestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/stacks/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_page_content(self):
+        response = self.client.get('/stacks/')
+        self.assertContains(response, "New Application")
+
+    def test_new_stack_page_status_code(self):
+        response = self.client.get('/stacks/new')
+        self.assertEquals(response.status_code, 200)
+
+    def test_new_stack_page_content(self):
+        response = self.client.get('/stacks/new')
+        self.assertContains(response, "<title>Cloud69</title>")
